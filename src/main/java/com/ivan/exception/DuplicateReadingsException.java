ddf80e09fca64062626f74a41ba5b6eba7a10987
ssssssergeenkov <itproger181920@gmail.com>
2024-01-29 18:59:04 +0300

    added a new Exception

---
+++ src/main/java/com/ivan/exception/DuplicateReadingsException.java
@@ -0,0 +1,8 @@
+package com.ivan.exception;
+
+public class DuplicateReadingsException extends RuntimeException{
+
+    public DuplicateReadingsException(String message) {
+        super(message);
+    }
+}
