0f8c3eeedf3f8bc28e3b56ca01f4a78073819ae2
ssssssergeenkov <itproger181920@gmail.com>
2024-04-05 23:22:05 +0300

    new README file + minor change

---
+++ src/main/java/com/ivan/MonitoringServiceApplication.java
@@ -0,0 +1,14 @@
+package com.ivan;
+
+/**
+ * MonitoringServiceApplication class with the main method to start the application by running the run method from ApplicationRunner.
+ */
+public class MonitoringServiceApplication {
+    /**
+     * MonitoringServiceApplication method to start the application.
+     */
+    public static void main(String[] args) {
+        ApplicationRunner.run();
+    }
+}
+// TODO: 05.04.2024 РЕАЛИЗУЙ DTO ЧТОБЫ ПРОЕКТ МОЖНО БЫЛО НЕСТЫДНО ЗАПУШИТЬ
