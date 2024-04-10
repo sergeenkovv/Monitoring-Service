baba3cbb300ac4874e67a3f5fd45c3860b29376b
ssssssergeenkov <itproger181920@gmail.com>
2024-04-11 00:05:35 +0300

    fix inaccuracy

--- src/main/java/com/ivan/service/impl/PlayerServiceImpl.java
+++ src/main/java/com/ivan/service/impl/PlayerServiceImpl.java
@@ -34,9 +34,9 @@ public class PlayerServiceImpl implements PlayerService {
     /**
      * Submits a new meter reading for a player.
      *
-     * @param playerId The ID of the player submitting the meter reading.
+     * @param playerId  The ID of the player submitting the meter reading.
      * @param meterType The type of the meter reading.
-     * @param counter The meter reading value.
+     * @param counter   The meter reading value.
      */
     public void submitMeterReading(Long playerId, String meterType, Integer counter) {
         if (!isValidMeterReadings(playerId, MeterType.valueOf(meterType))) {
@@ -57,8 +57,8 @@ public class PlayerServiceImpl implements PlayerService {
      * Retrieves meter readings for a player based on the specified year and month.
      *
      * @param playerId The ID of the player for whom to retrieve meter readings.
-     * @param year The year for which to retrieve meter readings.
-     * @param month The month for which to retrieve meter readings.
+     * @param year     The year for which to retrieve meter readings.
+     * @param month    The month for which to retrieve meter readings.
      * @return List of meter readings for the specified player in the specified year and month.
      */
     public List<MeterReading> getMeterReadingsByMonth(Long playerId, Integer year, Integer month) {
@@ -78,22 +78,15 @@ public class PlayerServiceImpl implements PlayerService {
     /**
      * Checks if the player has already submitted a meter reading of the same type in the current month.
      *
-     * @param playerId The ID of the player to check.
+     * @param playerId  The ID of the player to check.
      * @param meterType The meter reading type to check.
      * @return True if a reading of the same type already exists for the player in the current month, false otherwise.
      */
     private boolean isValidMeterReadings(Long playerId, MeterType meterType) {
         List<MeterReading> allByPlayerUsername = meterReadingDao.findAllByPlayerId(playerId);
 
-        for (MeterReading reading : allByPlayerUsername) {
-            if (reading.getMeterType() == meterType &&
-                YearMonth.from(reading.getDate()).equals(YearMonth.now())) {
-                return false;
-            }
-        }
-        return true;
+        return allByPlayerUsername.stream()
+                .noneMatch(reading -> reading.getMeterType() == meterType &&
+                                      YearMonth.from(reading.getDate()).equals(YearMonth.now()));
     }
 }
-//        return allByPlayerUsername.stream()
-//                .anyMatch(reading -> reading.getMeterType() == meterType &&
-//                YearMonth.from(reading.getDate()).equals(YearMonth.now()));
